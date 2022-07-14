e42631d64cbb757882b27b810b97b2afa87a251d
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-14 14:00:44 -0700

    implement the div opcode

--- opcodes2.c
+++ opcodes2.c
@@ -65,3 +65,29 @@ void _sub(stack_t **dbly, unsigned int count_line)
 	curr->n -= (*dbly)->n;
 	_pop(dbly, count_line);
 }
+/**
+ * _div - divides the second top element of the stack by
+ * the top element of the stack
+ * @dbly: head of the linked list
+ * @count_line: line number
+ * Return: void.
+ */
+void _div(stack_t **dbly, unsigned int count_line)
+{
+	int i = 0;
+	stack_t *curr = NULL;
+
+	curr = *dbly;
+	for (; curr != NULL; curr = curr->next, i++)
+		;
+	if (i < 2)
+	{
+		dprintf(2, "L%u: ", count_line);
+		dprintf(2, "can't div, stack too short\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	curr = (*dbly)->next;
+	curr->n /= (*dbly)->n;
+	_pop(dbly, count_line);
+}
